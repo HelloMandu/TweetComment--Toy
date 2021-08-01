a24e92deb8538a87665b90220b33536f88516694
HelloMandu <tjdals6695@gmail.com>
2021-08-01 23:29:50 +0900

    create error 수정해야함

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -9,9 +9,8 @@ const Avatar = memo(({ url, name }: AvatarProps) => (
   <div>
     {!!url ? (
       <img src={url} alt="avatar" className="avatar-img" />
-    ) : (
-      <div className="avatar-txt">{name.charAt(0)}</div>
-    )}
+    ) : // <div className="avatar-txt">{name.charAt(0)}</div>
+    null}
   </div>
 ));
 
