cbce68d32cbeceea4a197b5522be34446603c6fd
HelloMandu <tjdals6695@gmail.com>
2022-05-29 00:44:43 +0900

    TweetController refactoring

--- backend/src/controller/tweet.controller.ts
+++ backend/src/controller/tweet.controller.ts
@@ -1,115 +1,108 @@
-import { mock_tweets } from '../data/mock_tweets';
 import { Request, Response } from 'express';
 import TweetRepository from '../repository/tweet.repository';
-import { TweetModel, TweetResult } from '../model/tweet.model';
 import UserRepository from '../repository/user.repository';
-import { mock_users } from '../data/mock_users';
+import { TweetModel, TweetResult } from '../model/tweet.model';
 
-const tweetRepository = new TweetRepository(mock_tweets);
-const userRepository = new UserRepository(mock_users);
+class TweetController {
+  private tweetRepository: TweetRepository;
+  private userRepository: UserRepository;
 
-const getTweets = async (req: Request, res: Response) => {
-  const username = req.query.username;
-  if (username && typeof username !== 'string') {
-    return res.status(403).json({ message: 'username is invalid' });
+  constructor(args:{
+    tweetRepository: TweetRepository,
+    userRepository: UserRepository
+  }) {
+    this.tweetRepository = args.tweetRepository;
+    this.userRepository = args.userRepository;
   }
 
-  const user = await (username && userRepository.findByUsername(username));
-  if (username && !user) {
-    return res.status(403).json({ message: 'username is invalid' });
-  }
+  async getTweets(req: Request, res: Response) {
+    const username = req.query.username;
+    if (username && typeof username !== 'string') {
+      return res.status(403).json({ message: 'username is invalid' });
+    }
 
-  const tweets: TweetModel[] = await (user
-    ? tweetRepository
+    const user = await (username && this.userRepository.findByUsername(username));
+    if (username && !user) {
+      return res.status(403).json({ message: 'username is invalid' });
+    }
+
+    const tweets: TweetModel[] = await (user
+      ? this.tweetRepository
         .getTweets()
         .then((tweets) => tweets.filter((tweet) => tweet.userId === user.id))
-    : tweetRepository.getTweets());
+      : this.tweetRepository.getTweets());
 
-  const tweetWithUser: TweetResult[] = await Promise.all(
-    tweets.map(async (tweet) => {
-      const user = await userRepository.findById(tweet.userId);
-      if (!user) {
-        // TODO: 해당 부분 어떻게 해결할지 생각해볼 것
-        throw Error('Can not find user');
-      }
-      const { name, username, url } = user;
-      return { tweet, user: { name, username, url } };
-    })
-  );
-  res.status(200).json(tweetWithUser);
-};
-
-const getTweetById = async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const tweet = await tweetRepository.getTweetById(id);
-  if (!tweet) {
-    return res.status(404).json({ message: `Tweet id(${id}) is not found` });
+    const tweetWithUser: TweetResult[] = await Promise.all(
+      tweets.map(async (tweet) => {
+        const user = await this.userRepository.findById(tweet.userId);
+        if (!user) {
+          // TODO: 해당 부분 어떻게 해결할지 생각해볼 것
+          throw Error('Can not find user');
+        }
+        const { name, username, url } = user;
+        return { tweet, user: { name, username, url } };
+      })
+    );
+    res.status(200).json(tweetWithUser);
   }
 
-  const user = await userRepository.findById(tweet.userId);
-
-  if (!user) {
-    throw Error('Can not find user');
-  }
-
-  res.status(200).json({ tweet, user });
-};
-
-const createTweet = async (req: Request, res: Response) => {
-  const { text } = req.body;
-
-  const { userId } = req;
-  if (!userId) {
-    return res.status(404).json({ message: `User ${userId} is not found` });
-  }
-
-  const user = await userRepository.findById(userId);
-
-  if (!user) {
-    return res.status(403).json({ message: 'username is invalid' });
-  }
-
-  const tweets = await tweetRepository.getTweets();
-
-  const newTweet: TweetModel = {
-    id: (tweets.length + 1).toString(),
-    text,
-    createdAt: new Date(),
-    userId,
+  async getTweetById(req: Request, res: Response) {
+    const id = req.params.id;
+    const tweet = await this.tweetRepository.getTweetById(id);
+    if (!tweet) {
+      return res.status(404).json({ message: `Tweet id(${id}) is not found` });
+    }
+    const user = await this.userRepository.findById(tweet.userId);
+    if (!user) {
+      throw Error('Can not find user');
+    }
+    res.status(200).json({ tweet, user });
   };
 
-  await tweetRepository.createTweet(newTweet);
+  async createTweet(req: Request, res: Response) {
+    const { text } = req.body;
+    const { userId } = req;
+    if (!userId) {
+      return res.status(404).json({ message: `User ${userId} is not found` });
+    }
+    const user = await this.userRepository.findById(userId);
+    if (!user) {
+      return res.status(403).json({ message: 'username is invalid' });
+    }
+    const tweets = await this.tweetRepository.getTweets();
+    const newTweet: TweetModel = {
+      id: (tweets.length + 1).toString(),
+      text,
+      createdAt: new Date(),
+      userId,
+    };
+    await this.tweetRepository.createTweet(newTweet);
+    res.status(201).json({ tweet: newTweet, user });
+  };
 
-  res.status(201).json({ tweet: newTweet, user });
-};
+  async updateTweet(req: Request, res: Response) {
+    const id = req.params.id;
+    const text: string = req.body.text;
+    const updated = await this.tweetRepository.updateTweet(id, text);
+    if (!updated) {
+      return res.status(404).json({ message: `Tweet id(${id}) is not found` });
+    }
+    const user = await this.userRepository.findById(updated.userId);
+    if (!user) {
+      throw Error('Can not find user');
+    }
+    res.status(200).json({ tweet: updated, user });
+  };
 
-const updateTweet = async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const text: string = req.body.text;
-  const updated = await tweetRepository.updateTweet(id, text);
+  async deleteTweet(req: Request, res: Response) {
+    const id = req.params.id;
+    const deleted = await this.tweetRepository.deleteTweet(id);
+    if (!deleted) {
+      return res.status(404).json({ message: `Tweet id(${id}) is not found` });
+    }
+    res.status(200).json(deleted);
+  };
+}
 
-  if (!updated) {
-    return res.status(404).json({ message: `Tweet id(${id}) is not found` });
-  }
+export default TweetController
 
-  const user = await userRepository.findById(updated.userId);
-
-  if (!user) {
-    throw Error('Can not find user');
-  }
-
-  res.status(200).json({ tweet: updated, user });
-};
-
-const deleteTweet = async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const deleted = await tweetRepository.deleteTweet(id);
-
-  if (!deleted) {
-    return res.status(404).json({ message: `Tweet id(${id}) is not found` });
-  }
-
-  res.status(200).json(deleted);
-};
-
-export { getTweets, getTweetById, createTweet, updateTweet, deleteTweet };
--- backend/src/router/tweet.router.ts
+++ backend/src/router/tweet.router.ts
@@ -1,24 +1,26 @@
 import express from 'express';
-import {
-  createTweet,
-  deleteTweet,
-  getTweetById,
-  getTweets,
-  updateTweet,
-} from '../controller/tweet.controller';
+import TweetController from '../controller/tweet.controller';
 import { tweetValidator } from '../middleware/validator/tweet.validator';
 import { isAuth } from '../middleware/auth';
+import TweetRepository from '../repository/tweet.repository';
+import { mock_tweets } from '../data/mock_tweets';
+import UserRepository from '../repository/user.repository';
+import { mock_users } from '../data/mock_users';
 
 const router = express.Router();
+const tweetController = new TweetController({
+  tweetRepository: new TweetRepository(mock_tweets),
+  userRepository: new UserRepository(mock_users)
+});
 
-router.get('/', isAuth, getTweets);
+router.get('/', isAuth, tweetController.getTweets.bind(tweetController));
 
-router.get('/:id', isAuth, getTweetById);
+router.get('/:id', isAuth, tweetController.getTweetById.bind(tweetController));
 
-router.post('/', isAuth, tweetValidator, createTweet);
+router.post('/', isAuth, tweetValidator, tweetController.createTweet.bind(tweetController));
 
-router.put('/:id', isAuth, tweetValidator, updateTweet);
+router.put('/:id', isAuth, tweetValidator, tweetController.updateTweet.bind(tweetController));
 
-router.delete('/:id', isAuth, deleteTweet);
+router.delete('/:id', isAuth, tweetController.deleteTweet.bind(tweetController));
 
 export default router;
