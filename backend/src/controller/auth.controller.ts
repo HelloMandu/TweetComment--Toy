80b778cc132cbf6782b4251fbbde998ee0bce602
HelloMandu <tjdals6695@gmail.com>
2022-05-29 00:48:52 +0900

    UserController refactoring

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -2,65 +2,66 @@ import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import UserRepository from '../repository/user.repository';
-import { mock_users } from '../data/mock_users';
 import { UserInfo } from '../model/userInfo';
 import { config } from '../config';
 
-const userRepository = new UserRepository(mock_users);
+class UserController {
+  private userRepository: UserRepository;
 
-const signUp = async (req: Request, res: Response) => {
-  const { username, password, name, email, url }: Omit<UserInfo, 'id'> = req.body;
-
-  const isExistUser = await userRepository.findByUsername(username);
-  if (isExistUser) {
-    return res.status(409).json({ message: `${username} already exists` });
+  constructor(args:{
+    userRepository: UserRepository
+  }) {
+    this.userRepository = args.userRepository;
   }
 
-  const hashed = await bcrypt.hash(password, config.bcrypt.saltRounds);
+  async signUp(req: Request, res: Response) {
+    const { username, password, name, email, url }: Omit<UserInfo, 'id'> = req.body;
+    const isExistUser = await this.userRepository.findByUsername(username);
+    if (isExistUser) {
+      return res.status(409).json({ message: `${username} already exists` });
+    }
+    const hashed = await bcrypt.hash(password, config.bcrypt.saltRounds);
+    const user = await this.userRepository.createUser({
+      username,
+      password: hashed,
+      name,
+      email,
+      url,
+    });
+    const token = this.createToken(user.id);
+    res.status(201).json({ token, user });
+  };
 
-  const user = await userRepository.createUser({
-    username,
-    password: hashed,
-    name,
-    email,
-    url,
-  });
+  async login(req: Request, res: Response) {
+    const { username, password }: Pick<UserInfo, 'username' | 'password'> = req.body;
+    const user = await this.userRepository.findByUsername(username);
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid user or password' });
+    }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(401).json({ message: 'Invalid user or password' });
+    }
+    const token = this.createToken(user.id);
+    res.status(201).json({ token, user });
+  };
 
-  const token = createToken(user.id);
-  res.status(201).json({ token, user });
-};
+  async me(req: Request, res: Response) {
+    const { userId } = req;
+    if (!userId) {
+      return res.status(404).json({ message: `User ${userId} is not found` });
+    }
+    const user = await this.userRepository.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: `User ${userId} is not found` });
+    }
+    return res.status(200).json(user);
+  };
 
-const login = async (req: Request, res: Response) => {
-  const { username, password }: Pick<UserInfo, 'username' | 'password'> = req.body;
+  createToken(id: string) {
+    return jwt.sign({ id }, config.jwt.secretKey, { expiresIn: config.jwt.expiresInSec });
+  };
+}
 
-  const user = await userRepository.findByUsername(username);
-  if (!user) {
-    return res.status(401).json({ message: 'Invalid user or password' });
-  }
 
-  const isValidPassword = await bcrypt.compare(password, user.password);
-  if (!isValidPassword) {
-    return res.status(401).json({ message: 'Invalid user or password' });
-  }
-
-  const token = createToken(user.id);
-  res.status(201).json({ token, user });
-};
-
-const me = async (req: Request, res: Response) => {
-  const { userId } = req;
-  if (!userId) {
-    return res.status(404).json({ message: `User ${userId} is not found` });
-  }
-  const user = await userRepository.findById(userId);
-  if (!user) {
-    return res.status(404).json({ message: `User ${userId} is not found` });
-  }
-  return res.status(200).json(user);
-};
-
-const createToken = (id: string) => {
-  return jwt.sign({ id }, config.jwt.secretKey, { expiresIn: config.jwt.expiresInSec });
-};
-
-export { signUp, login, me };
+export default UserController;
--- backend/src/router/auth.router.ts
+++ backend/src/router/auth.router.ts
@@ -1,14 +1,19 @@
 import express from 'express';
-import { login, me, signUp } from '../controller/auth.controller';
 import { validateCredential, validateSignUp } from '../middleware/validator/auth.validator';
 import { isAuth } from '../middleware/auth';
+import UserController from '../controller/auth.controller';
+import UserRepository from '../repository/user.repository';
+import { mock_users } from '../data/mock_users';
 
 const router = express.Router();
+const userController = new UserController({
+  userRepository: new UserRepository(mock_users)
+})
 
-router.post('/signUp', validateSignUp, signUp);
+router.post('/signUp', validateSignUp, userController.signUp.bind(userController));
 
-router.post('/login', validateCredential, login);
+router.post('/login', validateCredential, userController.login.bind(userController));
 
-router.get('/me', isAuth, me);
+router.get('/me', isAuth, userController.me.bind(userController));
 
 export default router;
